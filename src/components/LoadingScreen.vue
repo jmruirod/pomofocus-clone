1e5045fd67af0d3898097da1836b383400283008
José Manuel Ruiz Rodríguez <jmruirod@gmail.com>
2025-04-08 16:01:03 +0200

    Update LoadingScreen component to use dynamic image source binding for improved asset handling.

--- src/components/LoadingScreen.vue
+++ src/components/LoadingScreen.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-red-800/80 flex h-screen w-screen gap-4 items-center justify-center">
-    <img src="/pomofocus-clone/images/icon-white2.png" alt="logo" class="h-15 w-15" />
+    <img :src="`/pomofocus-clone/images/icon-white2.png`" alt="logo" class="h-15 w-15" />
     <p class="text-2xl font-bold text-white/80 animate-pulse">Cargando...</p>
   </div>
 </template>
